d1166cd467d505796a0dd8860cbbff0b6b44d39c
suleymazengin <suleymanmertzengin@gmail.com>
2021-02-23 14:47:50 +0300

    initialized project

---
+++ src/main/java/com/intengo/intengo/IntengoApplication.java
@@ -0,0 +1,13 @@
+package com.intengo.intengo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntengoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntengoApplication.class, args);
+	}
+
+}
